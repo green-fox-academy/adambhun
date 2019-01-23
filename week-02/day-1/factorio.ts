3127402a9c5ff09b4085d98ac5bed3e86dd52cce
Adam Bene <adambhun@gmail.com>
2019-01-23 08:58:49 +0100

    day-1

--- week-02/day-1/factorio.ts
+++ week-02/day-1/factorio.ts
@@ -4,15 +4,11 @@
 //    that returns it's input's factorial
 
 function factorio(num: number){
-    let nums: number[];
-    nums = [];
     let accu: number = 1;
-    for (let index = 1; index < num+1; index++) {  //creates array
-        nums.push(index); 
+    for (let index = 1; index < num+1; index++) {
         accu *= index;
     }
     console.log(accu);
-                  //rest parameter | note: spread may be better
 }
 
 factorio(5);
