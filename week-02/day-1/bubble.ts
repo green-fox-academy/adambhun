9a33af929a0a5fd6c34104b0bafc7a98a0d5a8e5
Adam Bene <youremail@example.com>
2019-01-27 21:56:40 +0100

    bubble

--- week-02/day-1/bubble.ts
+++ week-02/day-1/bubble.ts
@@ -2,6 +2,18 @@
 //  Returns a list where the elements are sorted in ascending numerical order
 //  Make a second boolean parameter, if it's `true` sort that list descending
 
+function bubble(params: number[]) {
+    return params.sort(function(a,b){return a-b});
+}
+
+function advancedBubble(params: number[],desc: boolean) {
+    if (desc = true) {
+        return params.sort(function(a,b){return b-a});
+    } else {
+    return params.sort(function(a,b){return a-b});
+    }
+}
+
 //  Example:
 console.log(bubble([34, 12, 24, 9, 5]));
 //  should print [5, 9, 12, 24, 34]
