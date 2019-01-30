cbe806b0965d74212ffc5cd8314eff139cd79989
Adam Bene <youremail@example.com>
2019-01-30 09:16:53 +0100

    "5trees"

---
+++ week-03/day-2/5-trees-homework.ts
@@ -0,0 +1,9 @@
+'use strict'
+export { }
+
+
+let tree1: any[] = ['mahagony', 'green', 15, 'male'];
+let tree2: any[] = ['rosewood', 'yellow', 50, 'female'];
+let tree3: any[] = ['oak', 'brown', 110, 'fale'];
+let tree4: any[] = ['maple', 'brown', 70, 'female'];
+let tree5: any[] = ['beech', 'red', 68, 'male'];
