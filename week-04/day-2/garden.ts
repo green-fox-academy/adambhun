35234211ac1fff02d1cef6f3d44ef424c64ea017
Adam Bene <youremail@example.com>
2019-02-06 10:09:31 +0100

    garden.ts

---
+++ week-04/day-2/garden.ts
@@ -0,0 +1,99 @@
+'use strict';
+export { };
+
+class Flower {
+  water: number = 0;
+  color: string;
+
+  constructor(color: string) {
+    this.color = color;
+  }
+}
+
+class Tree {
+  water: number = 0;
+  color: string;
+
+  constructor(color: string) {
+    this.color = color;
+  }
+}
+
+class Garden {
+  flowers: Flower[] = [];
+  trees: Tree[] = [];
+
+  constructor(flowers: Flower[], trees: Tree[]) {
+    this.flowers = flowers;
+    this.trees = trees;
+  }
+  needsWater: number = 0;
+  finalwater: number = 0;
+
+  watering(waterAmount: number) {
+
+    this.flowers.forEach(element => {
+      //check if needs water
+      if (element.water < 5) {
+        console.log(`The ${element.color} Flower needs water`); 
+        this.needsWater++;
+        return this.needsWater;       
+      } else {
+        console.log(`The ${element.color} Flower doesn't need water`);        
+      }
+    });
+    
+    this.trees.forEach(element => {
+      if (element.water < 10) {
+        console.log(`The ${element.color} Tree needs water`);  
+        this.needsWater++;
+        return this.needsWater;      
+      } else {
+        console.log(`The ${element.color} Tree doesn't need water`);        
+      }
+    });
+
+    //start watering
+    this.finalwater = waterAmount / this.needsWater;
+
+    this.flowers.forEach(element => {
+      if (element.water < 5) {
+        element.water += (this.finalwater * 0.75);
+        return element.water;
+      }
+    });
+
+    this.trees.forEach(element => {
+      if (element.water < 10) {
+        element.water += (this.finalwater * 0.4);
+        return element.water;
+      }
+    });
+
+    this.flowers.forEach(element => {
+      console.log(element.color, element.water);      
+    });
+
+    this.trees.forEach(element => {
+      console.log(element.color, element.water);      
+    });
+  }
+}
+
+let yellow = new Flower('yellow');
+let blue = new Flower('blue');
+let purple = new Tree('purple');
+let orange = new Tree('orange');
+
+let garden1 = new Garden([yellow, blue], [purple, orange]);
+
+//water those in need, div equally
+//SOLUTION: if plant needs to be watered increment needsWater by 1, then div water by counter, then water plants
+
+garden1.watering(40);
+
+garden1.watering(70);
+
+garden1.watering(200);
+
+garden1.watering(30);
