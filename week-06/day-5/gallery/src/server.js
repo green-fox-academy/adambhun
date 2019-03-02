da466d1dfa486261d0b270169ea144340727ceb6
Adam Bene <youremail@example.com>
2019-03-02 23:30:21 +0100

    css practice

--- week-06/day-5/gallery/src/server.js
+++ week-06/day-5/gallery/src/server.js
@@ -5,8 +5,12 @@ const app = express();
 const PORT = 3000;
 app.set('view engine', 'ejs');
 
+app.use('/assets/css', express.static('assets/css'));
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
