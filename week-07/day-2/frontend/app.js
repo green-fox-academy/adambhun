e83181775133bfa4dd4099a5cf2cb196bcd76677
Adam Bene <youremail@example.com>
2019-03-05 16:16:19 +0100

    greeter done, append 50%

--- week-07/day-2/frontend/app.js
+++ week-07/day-2/frontend/app.js
@@ -11,23 +11,38 @@ app.get('/', (req, res) => {
 });
 
 app.get('/doubling', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.sendFile(path.join(__dirname, 'index.html'));
   var input = req.query.input;
-  if (input === undefined) {
+  input === undefined ?
     res.json({
       error: "Please provide an input!"
-    });
-  } else {
+    }) :
     res.json({
       received: input,
       result: input * 2
     });
-  }
 });
 
+app.get('/greeter', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  var name = req.query.name;
+  var title = req.query.title;
+  name === undefined && title === undefined ?
+    res.json({ error: `Please provide a name and a title!` }) :
+  name === undefined ?
+    res.json({ error: `Please provide a name!` }) :
+    title === undefined ?
+      res.json({ error: `Please provide a title!` }) :
+        res.json({ welcome_message: `Oh, hi there ${name}, my dear ${title}!` });
+});
 
+app.get('/appenda/:appendable', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  var input = req.query.appendable;
+  input === undefined ?
+  res.json({error: "404"}) :
+  res.json({appended: `${input}a`});
+});
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
