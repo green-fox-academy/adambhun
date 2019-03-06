e9126984893a8cbda24adf25825e5de9c12faaa2
Adam Bene <youremail@example.com>
2019-03-06 22:59:17 +0100

    nytimes ready -permalink

---
+++ week-07/day-3/NYtimes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let httpRequest = new XMLHttpRequest();
+const call = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=apollo11&fq=headline&fq=snippet&fq=pub_date&api-key=Yj5VP94mNwavL7u5fPldNh3yAozLSYPY";
+let payload = "";
+
+
+httpRequest.open('GET', call, true);
+httpRequest.send(null);
+
+httpRequest.onload = () => {
+  payload = JSON.parse(httpRequest.responseText);
+  console.log(payload);
+  
+    let lst = document.createElement("li");
+    lst.innerHTML = payload.response.docs[0].headline.main;
+    let lst2 = document.createElement("li");
+    lst2.innerHTML = payload.response.docs[0].snippet;
+    let lst3 = document.createElement("li");
+    lst3.innerHTML = payload.response.docs[0].pub_date;
+    document.querySelector("body").appendChild(lst);
+    document.querySelector("body").appendChild(lst2);
+    document.querySelector("body").appendChild(lst3);
+}
