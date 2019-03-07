729c596a05ee589850ed81d46f29bee694d7e3e0
Adam Bene <youremail@example.com>
2019-03-08 00:28:04 +0100

    got table, but looks s#!t

--- week-07/day-4/bookstore/assets/frontend.js
+++ week-07/day-4/bookstore/assets/frontend.js
@@ -9,7 +9,7 @@ button1.onclick = () => {
   httpRequest.open('GET', 'http://localhost:3000/book_name', true);
   httpRequest.onload = (send) => {
     payload = JSON.parse(send.target.responseText);
-    
+
     for (let index = 0; index < payload.length; index++) {
       let lst = document.createElement("li");
       lst.innerHTML = payload[index].book_name;
@@ -24,7 +24,7 @@ button2.onclick = () => {
   httpRequest.open('GET', 'http://localhost:3000/authors_name', true);
   httpRequest.onload = (send) => {
     payload = JSON.parse(send.target.responseText);
-    
+
     for (let index = 0; index < payload.length; index++) {
       let lst = document.createElement("li");
       lst.innerHTML = payload[index].aut_name;
@@ -38,9 +38,26 @@ const gettable = document.querySelector('#get_table');
 gettable.onclick = () => {
   httpRequest.open('GET', 'http://localhost:3000/table', true);
   httpRequest.onload = (send) => {
-    console.log(send.target.responseText);
-    
     payload = JSON.parse(send.target.responseText);
+    for (let index = 0; index < payload.length; index++) {
+      let trow = document.createElement("tr");
+      document.querySelector("tr").appendChild(trow);
+      let cell1 = document.createElement("td");
+      cell1.innerHTML = payload[index].book_name;
+      document.querySelector("tr").appendChild(cell1);
+      let cell2 = document.createElement("td");
+      cell2.innerHTML = payload[index].aut_name;
+      document.querySelector("tr").appendChild(cell2);
+      let cell3 = document.createElement("td");
+      cell3.innerHTML = payload[index].cate_descrip;
+      document.querySelector("tr").appendChild(cell3);
+      let cell4 = document.createElement("td");
+      cell4.innerHTML = payload[index].pub_name;
+      document.querySelector("tr").appendChild(cell4);
+      let cell5 = document.createElement("td");
+      cell5.innerHTML = payload[index].book_price;
+      document.querySelector("tr").appendChild(cell5);
+    };
   }
   httpRequest.send();
 }
--- week-07/day-4/bookstore/bookstore.js
+++ week-07/day-4/bookstore/bookstore.js
@@ -56,7 +56,7 @@ app.get('/authors_name', (req, res) => {
 });
 
 app.get('/table', (req, res) => {
-  conn.query('SELECT book_name, book_price FROM book_mast LEFT JOIN category ON book_mast.cate_id=category.cate_id LEFT JOIN author ON book_mast.aut_id=author.aut_id LEFT JOIN publisher ON book_mast.pub_id=publisher.pub_id;', (err, rows) => {
+  conn.query('SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast LEFT JOIN category ON book_mast.cate_id=category.cate_id LEFT JOIN author ON book_mast.aut_id=author.aut_id LEFT JOIN publisher ON book_mast.pub_id=publisher.pub_id;', (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).send();
