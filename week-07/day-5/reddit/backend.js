f4372f98046d45718121caa2705268e565725363
Adam Bene <youremail@example.com>
2019-03-08 15:37:03 +0100

    correction

--- week-07/day-5/reddit/backend.js
+++ week-07/day-5/reddit/backend.js
@@ -24,11 +24,12 @@ conn.connect((err) => {
   console.log('DB is connected');
 }); 
 // conn.end();
-
+//HOME
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/views/index.html'));
 });
 
+//POST SOMETHING
 app.post('/posts', (req, res) => {  
   let title = req.query.title;
   let url = req.query.url;
@@ -43,9 +44,24 @@ app.post('/posts', (req, res) => {
     }
     res.send(rows);
     res.status(200).send();
+    // console.log(rows);
+  });
+});
+
+//SHOW POSTS
+app.get('/posts', (req, res) => {
+  conn.query('SELECT title, url, posted_at, score, vote FROM posts LEFT JOIN relations ON posts.id=relations.p_id LEFT JOIN users ON users.id=relations.u_id;', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send();
+      return;
+    }
+    res.send(rows);
+    res.status(200).send();
     console.log(rows);
   });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
