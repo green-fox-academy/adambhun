0d5d2f327d6fecb4af08ef62ee834b00ce85cbe7
Adam Bene <youremail@example.com>
2019-03-08 09:55:51 +0100

    html <-> frontend

---
+++ week-07/day-5/reddit/backend.js
@@ -0,0 +1,70 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const mysql = require('mysql');
+const path = require('path');
+app.use("/assets", express.static("assets"));
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.log(err);
+    console.error();
+    return;
+  }
+  console.log('DB is connected');
+}); 
+// conn.end();
+
+app.get('/hello', (req, res) => {
+  res.sendFile(path.join(__dirname, './assets/bookstore.html'));
+});
+
+app.get('/book_name', (req, res) => {
+  conn.query('SELECT book_name FROM book_mast;', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send();
+      return;
+    }
+    res.send(rows);
+    console.log(rows);
+  });
+});
+
+app.get('/authors_name', (req, res) => {
+  conn.query('SELECT aut_name FROM author;', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send();
+      return;
+    }
+    res.send(rows);
+    console.log(rows);
+  });
+});
+
+app.get('/table', (req, res) => {
+  conn.query('SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast LEFT JOIN category ON book_mast.cate_id=category.cate_id LEFT JOIN author ON book_mast.aut_id=author.aut_id LEFT JOIN publisher ON book_mast.pub_id=publisher.pub_id;', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send();
+      return;
+    }
+    res.send(rows);
+    console.log(rows);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
---
+++ week-07/day-5/reddit/public/assets/js/frontend.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// let httpRequest = new XMLHttpRequest();
+// let payload = "";
+
+let lst = document.createElement("h2");
+lst.innerHTML = "alsdkjfélkj";
+document.querySelector("h1").appendChild(lst);
+
+// httpRequest.open('GET', 'http://localhost:3000/hello', true);
+// httpRequest.onload = (send) => {
+//   payload = JSON.parse(send.target.responseText);
+// }
+// httpRequest.send();
+// }
