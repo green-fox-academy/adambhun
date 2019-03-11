7ff16b1fb9026716bcde5c7dfc4bfbab0f05189b
Adam Bene <youremail@example.com>
2019-03-11 08:54:30 +0100

    sg

--- week-06/day-5/gallery/main.js
+++ week-06/day-5/gallery/main.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+app.use('/assets', express.static('assets'))
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log('Serving @ 3k');
+});
