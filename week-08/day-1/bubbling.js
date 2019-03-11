8463d7740c8f1ec8c45864200b862765571b692f
Adam Bene <youremail@example.com>
2019-03-11 14:05:44 +0100

    delay ready, bubblign copied

--- week-08/day-1/bubbling.js
+++ week-08/day-1/bubbling.js
@@ -1,13 +1,36 @@
 'use strict';
 
-const insp = document.querySelector('.img-inspector');
-const nav = document.querySelector('nav');
+//JUDIT KÓDJA ALAPJÁN
+//JUDIT KÓDJA ALAPJÁN
 
-const fire = (e) => {
-  console.log('aadakj');
-  insp.setAttribute('class', 'in');
-};
-
-nav.addEventListener('click', fire);
-
-p:nth-child(2)
+document.addEventListener('DOMContentLoaded', () => {
+  let navBar = document.getElementsByTagName('nav')[0];
+  let img = document.querySelector('.img-inspector');
+  let imgSize = 100;
+  let imgPosition = {
+    x: 0,
+    y: 0
+  }
+  
+  navBar.addEventListener('click', (e) => {
+    let dataAction = e.target.dataset.action;
+    let dataDirection = e.target.dataset.direction;
+    if(dataAction === 'zoom'){
+      dataDirection === 'in' ? imgSize *= 1.2 : imgSize *= 0.8;
+      img.setAttribute('style', `background-size: ${imgSize}%`)
+    }else if(dataAction = 'move'){
+      
+      if(dataDirection === 'up'){
+        imgPosition.y -= 10;
+      }else if(dataDirection === 'down'){
+        imgPosition.y += 10;
+      }else if(dataDirection === 'right'){
+        imgPosition.x += 10;
+      }else if(dataDirection === 'left'){
+        imgPosition.x -= 10;
+      }
+      
+      img.setAttribute('style', `background-position: ${imgPosition.x}px ${imgPosition.y}px;`);
+    }    
+  })
+})
