c1d921bd4cf4b2ef16548924388a5911e8736281
Adam Bene <youremail@example.com>
2019-03-12 17:35:07 +0100

    reworked sql, added delete

--- week-07/day-5/reddit/backend.js
+++ week-07/day-5/reddit/backend.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//FIXME: HEADERS
+//FIXME: ACCEPT JSON
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -8,6 +11,7 @@ const mysql = require('mysql');
 const path = require('path');
 app.use("/public", express.static("public"));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -33,9 +37,13 @@ app.get('/', (req, res) => {
 
 //POST SOMETHING
 app.post('/posts', (req, res) => {  
+  
+  //users have to register first, so only known users post
   let title = req.body.title;
   let url = req.body.url;
-  conn.query(`INSERT INTO posts (title, url) VALUES ('${title}', '${url}');`, (err, rows) => {
+  let owner = req.body.owner;
+  let owner_id = req.body.owner_id;
+  conn.query(`INSERT INTO posts (title, url, owner, owner_id) VALUES ('${title}', '${url}', '${owner}', '${owner_id}');`, (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).send();
@@ -48,8 +56,7 @@ app.post('/posts', (req, res) => {
 
 //SHOW POSTS
 app.get('/posts', (req, res) => {
-  // SELECT title, url, posted_at, score, vote FROM posts LEFT JOIN relations ON posts.id=relations.p_id LEFT JOIN users ON users.id=relations.u_id
-  conn.query('SELECT title, url, posted_at, score, vote FROM posts;', (err, rows) => {
+  conn.query('SELECT title, url, posted_at, score, owner FROM posts;', (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).send();
@@ -63,7 +70,7 @@ app.get('/posts', (req, res) => {
 //UPVOTE
 app.put('/posts/:id/upvote', (req, res) => {
   const pId = req.params.id;
-  conn.query(`UPDATE posts SET vote = vote + 1 WHERE id = ${pId};`, (err, rows) => {
+  conn.query(`UPDATE posts SET score = score + 1 WHERE id = ${pId};`, (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).send();
@@ -77,7 +84,7 @@ app.put('/posts/:id/upvote', (req, res) => {
 //DOWNVOTE
 app.put('/posts/:id/downvote', (req, res) => {
   const pId = req.params.id;
-  conn.query(`UPDATE posts SET vote = vote - 1 WHERE id = ${pId};`, (err, rows) => {
+  conn.query(`UPDATE posts SET score = score - 1 WHERE id = ${pId};`, (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).send();
@@ -88,6 +95,19 @@ app.put('/posts/:id/downvote', (req, res) => {
   });
 });
 
+//DELETE
+app.delete('/posts/:id', (req, res) => {
+  const pId = req.params.id;
+  conn.query(`DELETE FROM posts WHERE id='${pId}';`, (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send();
+      return;
+    }
+    res.status(200).send(rows);
+    // console.log(rows);
+  });
+});
 
 
 app.listen(PORT, () => {
