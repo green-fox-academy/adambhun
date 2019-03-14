caac6f1723e201cedef31ea296d0b630eba52a30
Adam Bene <youremail@example.com>
2019-03-14 10:48:31 +0100

    req res headers set

--- week-07/day-5/reddit/backend.js
+++ week-07/day-5/reddit/backend.js
@@ -1,4 +1,8 @@
 'use strict';
+//FIXME: error handling
+//FIXME: add users
+//FIXME: session
+//FIXME: breaks
 
 require('dotenv').config();
 const express = require('express');
@@ -9,7 +13,7 @@ const path = require('path');
 let accepts = require('accepts');
 app.use("/public", express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -26,7 +30,7 @@ conn.connect((err) => {
     return;
   }
   console.log('DB is connected');
-}); 
+});
 // conn.end();
 //HOME
 app.get('/', (req, res) => {
@@ -35,15 +39,12 @@ app.get('/', (req, res) => {
 
 //POST SOMETHING
 app.post('/posts', (req, res) => {
-  req.accepts('application/json');
-  res.set('Content-type', 'application/json');
-  let accept = accepts(req);
-  if (accept.type('application/json') === 'application/json') {
-    console.log('success');
-    
-  // console.log( req.accepts('application/json'));
-  // req.get('Content-type') === 'application/json' ?
-  //users have to register first, so only known users post
+  if (req.accepts('application/json') === false) {
+    console.log('wrong request header');
+    if (req.is('application/json') !== 'application/json') {
+      console.log('header: wrong content type');
+    }
+  }
   let title = req.body.title;
   let url = req.body.url;
   let owner = req.body.owner;
@@ -54,24 +55,27 @@ app.post('/posts', (req, res) => {
       res.status(500).send();
       return;
     }
+    res.set('Content-type', 'application/json');
     res.status(200).send(rows);
-    // console.log(rows);
+    console.log(rows);
   });
-} else {
-  console.log('epic fail');
-  
-}
 });
 
 //SHOW POSTS
 app.get('/posts', (req, res) => {
-  req.accepts('application/json');
+  if (req.accepts('application/json') === false) {
+    console.log('wrong request header');
+    if (req.is('application/json') !== 'application/json') {
+      console.log('header: wrong content type');
+    }
+  }
   conn.query('SELECT title, url, posted_at, score, owner FROM posts;', (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).send();
       return;
     }
+    res.set('Content-type', 'application/json');
     res.status(200).send(rows);
     // console.log(rows);
   });
@@ -79,7 +83,13 @@ app.get('/posts', (req, res) => {
 
 //UPVOTE
 app.put('/posts/:id/upvote', (req, res) => {
-  req.accepts('application/json');
+  if (req.accepts('application/json') === false) {
+    console.log('wrong request header');
+    if (req.is('application/json') !== 'application/json') {
+      console.log('header: wrong content type');
+    }
+  }
+
   const pId = req.params.id;
   conn.query(`UPDATE posts SET score = score + 1 WHERE id = ${pId};`, (err, rows) => {
     if (err) {
@@ -87,6 +97,7 @@ app.put('/posts/:id/upvote', (req, res) => {
       res.status(500).send();
       return;
     }
+    res.set('Content-type', 'application/json');
     res.status(200).send(rows);
     // console.log(rows);
   });
@@ -94,7 +105,12 @@ app.put('/posts/:id/upvote', (req, res) => {
 
 //DOWNVOTE
 app.put('/posts/:id/downvote', (req, res) => {
-  req.accepts('application/json');
+  if (req.accepts('application/json') === false) {
+    console.log('wrong request header');
+    if (req.is('application/json') !== 'application/json') {
+      console.log('header: wrong content type');
+    }
+  }
   const pId = req.params.id;
   conn.query(`UPDATE posts SET score = score - 1 WHERE id = ${pId};`, (err, rows) => {
     if (err) {
@@ -102,6 +118,7 @@ app.put('/posts/:id/downvote', (req, res) => {
       res.status(500).send();
       return;
     }
+    res.set('Content-type', 'application/json');
     res.status(200).send(rows);
     // console.log(rows);
   });
@@ -109,7 +126,12 @@ app.put('/posts/:id/downvote', (req, res) => {
 
 //DELETE
 app.delete('/posts/:id', (req, res) => {
-  req.accepts('application/json');
+  if (req.accepts('application/json') === false) {
+    console.log('wrong request header');
+    if (req.is('application/json') !== 'application/json') {
+      console.log('header: wrong content type');
+    }
+  }
   const pId = req.params.id;
   conn.query(`DELETE FROM posts WHERE id='${pId}';`, (err, rows) => {
     if (err) {
@@ -117,6 +139,7 @@ app.delete('/posts/:id', (req, res) => {
       res.status(500).send();
       return;
     }
+    res.set('Content-type', 'application/json');
     res.status(200).send(rows);
     // console.log(rows);
   });
@@ -124,7 +147,13 @@ app.delete('/posts/:id', (req, res) => {
 
 //MODIFY POST
 app.delete('/posts/:id', (req, res) => {
-  req.accepts('application/json');
+  if (req.accepts('application/json') === false) {
+    console.log('wrong request header');
+    if (req.is('application/json') !== 'application/json') {
+      console.log('header: wrong content type');
+    }
+  }
+
   const id = req.params.id;
   const title = req.body.title;
   const url = req.body.url;
@@ -134,6 +163,7 @@ app.delete('/posts/:id', (req, res) => {
       res.status(500).send();
       return;
     }
+    res.set('Content-type', 'application/json');
     res.status(200).send(rows);
     // console.log(rows);
   });
